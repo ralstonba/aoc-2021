aaeecfe43251df1e3c43b2b06517281edc2573e1
Brendan Ralston <brendan@ralston.io>
2021-12-01 20:55:12 -0500

    Add day 1 solution and badges

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,33 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input
+            .map { it.toInt() }
+            .windowed(2)
+            .map { it.last() > it.first() }.count { it }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input
+            .asSequence()
+            .map { it.toInt() }
+            .windowed(3)
+            .map { it ->  it.sum() }
+            .windowed(2)
+            .map { it.last() > it.first() }
+            .count { it }
     }
 
-    // test if implementation meets criteria from the description, like:
+    // test part 1 example matches provided
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
 
+    // solve part 1
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    println("Part 1 solution: ${part1(input)}")
+
+    // test part 2 example matches provided
+    check(part2(testInput) == 5)
+
+    // solve part 2
+    println("Part 2 solution: ${part2(input)}")
 }
